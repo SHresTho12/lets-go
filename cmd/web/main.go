7886409adad6e0b7b10b8aa9ec0319eb0e899b98
SHresTho12 <talimul12bari@gmail.com>
2023-08-08 18:44:08 +0600

    adds logger for requests

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -54,20 +54,13 @@ func main() {
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
 	}
-	// mux := http.NewServeMux()
-	// fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// // Use the mux.Handle() function to register the file server as the handler for all URL paths that start with "/static/". For matching paths, we strip the "/static" prefix before the request reaches the file server.
-	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(), // Call the new app.routes() method
 
 	}
-
-	// mux.HandleFunc("/", app.home)
-	// mux.HandleFunc("/snippet/view", app.snippetView)
-	// mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	infoLog.Printf("Starting server on %s", *addr)
 	// Call the ListenAndServe() method on our new http.Server struct.     err := srv.ListenAndServe()
